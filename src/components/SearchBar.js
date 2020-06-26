51a774785c0ed19470bea1b901d32dc49bdfa19c
Carl Olson <colson9@student.cscc.edu>
2020-06-26 11:53:32 -0400

    Search Icon Replaced with ominous skull

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
 
 const SearchBar = () => {
   return <View style={styles.background}>
-    <Text>Search Bar</Text>
+    <FontAwesome5 name="skull" size={34} color="black" />
+    {/* <Feather name="search" size={34} color="black" /> */}
+    <Text style={{fontSize: 30}}>  Search Bar</Text>
   </View>
 };
 
 const styles = StyleSheet.create({
   background: {
+    flexDirection: 'row',
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5, 
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    marginTop: 10,
+  },
 
-  }
 });
 
 export default SearchBar;
