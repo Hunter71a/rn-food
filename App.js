f9641a6fcda871d3bf7db9b25c3a78e2fe657d04
Carl Olson <colson9@student.cscc.edu>
2020-06-25 23:54:37 -0400

    install stack navigator

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Shut up CHUMP! Time to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
