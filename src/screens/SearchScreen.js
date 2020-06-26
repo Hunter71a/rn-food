f863660bb24a11429bf260b6f05c910bba38a03a
Carl Olson <colson9@student.cscc.edu>
2020-06-26 17:05:44 -0400

    update api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import { set } from 'react-native-reanimated';
 
 
 const SearchScreen = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchYelpApi = async () => {
-    const response = await yelp.get('/search', {
-      paramms: {
-        limit: 50,
-        term: query,
-        location: 'columbus'
-
-      }
-    });
-   
-   setResults(response.data.businesses);
+  const searchYelpApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'columbus'
+        }
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    } catch (error) {
+      console.log(error);
+      setErrorMessage('Soory. No results for you.')
+    }
   };
+
+  // Call searchAPI when componenet first rendered
+ // searchYelpApi('pasta');  don't do this --creates infinite loop!
 
   return <View style={styles.pageStyle}>
     <SearchBar
       query={query}
-      onQueryChange={newQuery => setQuery(newQuery)}
-      submitQuery={query => searchYelpApi(query)}
+      onQueryChange={setQuery}
+      submitQuery={() => searchYelpApi(query)}
+
+    // more verbose but equivalent syntax!!
+    // onQueryChange={newQuery => setQuery(newQuery)}
+    // submitQuery={() => searchYelpApi()}
 
     />
-    <Text>Search Screen</Text>
+    {errorMessage ? <Text>{errorMessage}</Text> : null}
     <Text> Searching for: {query}</Text>
     <Text>We Have Found {results.length} results</Text>
   </View>
