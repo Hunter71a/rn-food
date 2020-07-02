8539541d41c6fb9de385d0e67b8b70a21b27fabe
Carl Olson <colson9@student.cscc.edu>
2020-07-02 18:34:41 -0400

    constant fixed to working

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer p49G3wdY9gx11mMFY5ar-zAwFVGMuW2L-uIVzUH0pOY7DFdXHFFjsJ1WgG--UO7kNeNnAj9u3tv6btNIHoDnVS4MXPgB0-es3ZDUYQZ2I0DyA7RPKluD8F4CnC_1XnYx'
+    Authorization: 'Bearer p49G3wdY9gx11mMFY5ar-zAwFVGMuW2L-uIVzUH0pOY7DFdXHFinvalidFjsJ1WgG--UO7kNeNnAj9u3tv6btNIHoDnVS4MXPgB0-es3ZDUYQZ2I0DyA7RPKluD8F4CnC_1XnYx'
   }
 });
